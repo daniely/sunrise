f3ceffa08394bb110652df2783a926354f88154b
Daniel <daniel.kiros@gmail.com>
2016-04-09 11:58:53 -0700

    hack together tenant update route

--- app/controllers/api/v1/tenants_controller.rb
+++ app/controllers/api/v1/tenants_controller.rb
@@ -13,4 +13,17 @@ class Api::V1::TenantsController < Api::V1::BaseController
 
     render json: { success: 1 }
   end
+
+  def update
+    tenant = Tenant.find(params[:id])
+
+    tenant.name = params[:tenant][:name]
+    tenant.description = params[:tenant][:description]
+
+    if tenant.save!
+      render json: { id: tenant.id, name: tenant.name, description: tenant.description }
+    else
+      render json: { success: 0 }
+    end
+  end
 end
